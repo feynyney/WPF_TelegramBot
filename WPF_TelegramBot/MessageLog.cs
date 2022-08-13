834503a127533bd0814a644816f615427599e743
Dmytro <feynyney@gmail.com>
2022-08-13 15:38:21 +0300

    Добавьте файлы проекта.

---
+++ WPF_TelegramBot/MessageLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_TelegramBot
+{
+    struct MessageLog
+    {
+        public string Time { get; set; }
+
+        public string Msg { get; set; }
+
+
+        public MessageLog(string Time, string Msg)
+        {
+            this.Time = Time;
+            this.Msg = Msg;
+        }
+    }
+}
---
+++ WPF_TelegramBot/TelegramMessageClient.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.Threading;
+
+
+namespace WPF_TelegramBot
+{
+    internal class TelegramMessageClient
+    {
+        private TelegramBotClient _botClient = new TelegramBotClient("5448195820:AAFr1OH_6camjeRqsMxilfW8AZyVajXDZRs");
+
+        private MainWindow w;
+
+        public ObservableCollection<MessageLog> BotMessageLog { get; set; }
+
+
+        public TelegramMessageClient(MainWindow W)
+        {
+            this.BotMessageLog = new ObservableCollection<MessageLog>();
+            this.w = W;
+
+            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+            {
+                if (update.Message is not { } message)
+                    return;
+
+                Console.WriteLine("---");
+                Debug.WriteLine("+++---");
+
+                string text = $"{DateTime.Now.ToLongTimeString()}: {message.Chat.FirstName} {message.Chat.Id} {message.Text}";
+
+                Debug.WriteLine($"{text} TypeMessage: {message.Type.ToString()}");
+
+                var messageText = message.Text;
+
+                w.Dispatcher.Invoke(() =>
+                {
+                    BotMessageLog.Add(
+                    new MessageLog(
+                        DateTime.Now.ToLongTimeString(), messageText.ToString()));
+                });
+            }
+
+            _botClient.StartReceiving(
+                   updateHandler: HandleUpdateAsync,
+                   pollingErrorHandler: HandlePollingErrorAsync
+               );
+        }
+
+
+        
+
+        public void SendMessage(string Text, string Id)
+        {
+            long id = Convert.ToInt64(Id);
+            _botClient.SendTextMessageAsync(id, Text);
+        }
+
+        Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
